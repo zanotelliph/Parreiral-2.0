1ff16203b862a4cbb404cd392ed9a0fea72b5298
Tiago Coletti <tiagoalfonsoneumann123@gmail.com>
2026-06-14 13:54:06 -0300

    fix: atualiza dependências, corrige seeders e adiciona tabela de sessões

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,11 +12,6 @@ class DatabaseSeeder extends Seeder
 
     public function run(): void
     {
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
-
         $this->call(ParreiralSeeder::class);
     }
 }
